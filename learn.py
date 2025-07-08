bf59ff3b437fe4e1b6481408524d1d58bed87ed0
DuckyBoi-XD <braedenjairsytan@icloud.com>
2025-07-08 18:48:35 +1200

    Finished casting, worked through the string tutorials, finished everything but methods

--- learn.py
+++ learn.py
@@ -25,39 +25,39 @@ print("# and the key to comments")
 print(" ")
 print("Forth lesson") # The fourth lesson covers on variables where we need to first assign a  value to them to use it in our code.
 
-x = 5
-y = "Raymont"
+var1 = 5
+var2 = "Raymont"
 
 # The lesson also shows how to print variables. Instead of print with ("_______"), we can just  put in the variable name in (___). 
 
-print(x)
-print(y)
+print(var1)
+print(var2)
 
 print(" ")
 print("Fifth lesson")  # The fifth lesson teaches how to use casting to print specific data types.
 
-x = str(7.2) # str means string which is the text of the variable, 7.2 would output as 7.2 and  'apple' would output to apple
+str1 = str(7.2) # str means string which is the text of the variable, 7.2 would output as 7.2 and  'apple' would output to apple
 
-y = int(7.2) # int mean integer which is a whole number not a decimal, 7.2 would output as 7  and 'apple' would output as an error due to it not being a number but a text
+int1 = int(7.2) # int mean integer which is a whole number not a decimal, 7.2 would output as 7  and 'apple' would output as an error due to it not being a number but a text
 
-z = float(7.2) # float means (I guess) floating which means that the output number would be the
+float1 = float(7.2) # float means (I guess) floating which means that the output number would be the
 # exact number (I think) and not text. So, 7.2 would output as 7.2 and 'apple' would output as an error due to it not being a number but a text
 
-print(x)
-print(y)
-print(z)
+print(str1)
+print(int1)
+print(float1)
 
 # When code becomes like this I like to play around with the code to potentioally make it better  or break it completely.
 
 X = 4.761 # This varible is to change all the other casting varibles quickly.
 
-x = str(X)
-y = int(X)
-z = float(X)
+str2 = str(X)
+int2 = int(X)
+float2 = float(X)
 
-print(x)
-print(y)
-print(z)
+print(str2)
+print(int2)
+print(float2)
 
 print(" ")
 print("Sixth lesson") # The sixth lesson talks about how to name variables. It's important to  understand how to properly name variables so errors won't occur.
@@ -88,45 +88,44 @@ print("Naming variables needs to follow these rules: You can use lower case lett
 print(" ")
 print("Seventh lesson")  # The seventh lesson teaches how to assign multiple values to variables in.
 
-x, y, z = 'mango', 'watermelon', 'pear' # This prosses is basically assigning a list of variables with a list of values.
+m, w, p = 'mango', 'watermelon', 'pear' # This prosses is basically assigning a list of variables with a list of values.
 
-print(x)
-print(y)
-print(z)
+print(m)
+print(w)
+print(p)
 
 # If you want to assign the variables as the anme value you just put '=' everywhere.
-
-x = y = z = 'strawberry'
-print(x)
-print(y)
-print(z)
+straw1 = straw2 = straw3 = 'strawberry'
+print(straw1)
+print(straw2)
+print(straw3)
 
 # If you want to reverse it and get the values from the variables you do something called unpacking.  
 fruits = ['mango', 'watermelon', 'pear'] # listing the values into a variable.
 
-x, y, z = fruits # assigning the variables to the valuesin the variable 'fruits'
+m1, w2, p2 = fruits # assigning the variables to the valuesin the variable 'fruits'
 
-print(x)
-print(y)
-print(z)
+print(m1) # This will print the first value of the variable 'fruits', which is 'mango'
+print(w2) # This will print the second value of the variable 'fruits', which is 'watermelon'
+print(p2) # This will print the third value of the variable 'fruits', which is 'pear'
 
 print(" ")
 print("Eighth lesson") # The eighth lesson teaches how to output variables, like text, in 'print' 
 
-x = 'people from the Hackclub community are really helpful!'
-print(x)
+Opinion = 'people from the Hackclub community are really helpful!'
+print(Opinion)
 
-a = 'People '# Notice there are spaces between the word and the quotation mark, this is to make sure that the words are spaced out when printed.
-b = 'from '
-c = 'the '
-d = 'Hackclub '
-e = 'community '
-f = 'are '
-g = 'really '
-h = 'helpful!'
-print(a, b, c, d, e, f, g, h) # This is using the ',' symbol to combine the string variables together to make a sentance.
+a11 = 'People '# Notice there are spaces between the word and the quotation mark, this is to make sure that the words are spaced out when printed.
+b11 = 'from '
+c11 = 'the '
+d11 = 'Hackclub '
+e11 = 'community '
+f11 = 'are '
+g11 = 'really '
+h11 = 'helpful!'
+print(a11, b11, c11, d11, e11, f11, g11, h11) # This is using the ',' symbol to combine the string variables together to make a sentance.
 
-print(a + b + c + d + e + f + g + h) # This can also be done with the '+' symbol.
+print(a11 + b11 + c11 + d11 + e11 + f11 + g11 + h11) # This can also be done with the '+' symbol.
 
 # With numbers, if you use the '+' symbol, the numbers will be added up instead of being combined.
 x = 42
@@ -134,8 +133,8 @@ y = 31
 print(x + y)
 
 # You can combine 2 variables together even when one is text and one is a number. This is because a number can be a string and a text is already a string.
-x = '42'
-y = 'Koji'
+x2 = '42'
+y2 = 'Koji'
 print(x, y) 
 
 print(" ")
@@ -485,3 +484,255 @@ print(random.triangular(1, 100, 10)) # Generates a random float between 1 - 100 
 print(" ")
 # The rest of the RM lessons are not really useful for my kind of knowledge, the functions are tagged with, 'Ued in statistics' and 'Used in theroy.
 #I decided to stop the RM lessons here since I already gotten a lot of information and functions for my game project.
+
+print("Twelfth lesson") # The twelfth lesson which talks about casting
+# Casting the fuction where you change the data type of a cariable to another
+# Pythin automatically assaignes a data type to a variables when you defind it
+
+x = 5
+y = 45.32
+z = "Hackclub"
+
+print(type(x)) # Prints the data type of x, which is int
+print(type(y)) # Prints the data type of y, which is float
+print(type(z)) # Prints the data type of z, which is str
+
+print(" ")
+
+# You can change the data type of a variable by using the casting function
+fx = float(x) # Changes the data type of x from int to float
+iy = int(y) # Changes the data type of y from float to int
+sz = str(x) # Changes the data type of x from int to str
+
+print(fx) # Prints the value of fx, which is 5.0
+print(type(fx)) # Prints the data type of fx, which is float
+print(iy) # Prints the value of iy, which is 45
+print(type(iy)) # Prints the data type of iy, which is int
+print(sz) # Prints the value of sz, which is '5'
+print(type(sz)) # Prints the data type of sz, which is str
+
+print(" ")
+
+print("Thirteenth lesson") # The thirteenth lesson talks about strings or str
+# Strings are a data type that is used to store text. 
+# Strings can be defined by using single quotes or double quotes. I think double quotes is better because you might need to use single quotes in a string
+# If you use single quotes in a string using singe quotes to define it, it will come out as an error.
+
+x = "Hackclub is awesome" # Defines a string using double quotes
+y = 'Hackclub is awesome' # Defines a string using single quotes
+print(x) # Prints the value of x
+print(y) # Prints the value of y
+# They will output as the same thing, so it doesn't matter which one you use
+
+print(" ")
+
+x = "Hackclub's programs are supportive" # This string uses single quotes in the string. Since I used double quotes to deifine it, it'a ok
+# y = 'Hackclub's programs are supportive'
+# # This string uses single quotes in the string. Since I used single quotes to define it, it will come out as an error
+print(x) # Prints the value of x
+
+# To assign a string to a variable you need to do this:
+X = "Hackclub" 
+Y = 'Hackclub'
+# Names variable at the start
+# Defines a string using double quotes or single quotes
+
+print(" ")
+
+# Milti-line strings are impornt when writing strings that are more than 1 line.
+x = """There are many types of coding languages
+Python
+Java
+C++
+and many more""" 
+# You can have string with multiple lines by using either triple single quite ''' or triple double quotes """
+# If you don't use tripe quotes, when you move to the next line, it will be considered as a new line of code
+print(x) # Prints the value of x, which is a multi-line string
+
+# String are arrays (sort of like lists)
+# Because string is like arrays you can access each caracter of a string using indexing
+x = "Hackclub"
+print(x[0]) # Prints the first character of the string, which is 'H'
+
+print(" ")
+
+# You can also use loops to access each character of a string
+# The tutorial also splits off the string lesson to the loop lesson. I will be going through the loop lesson later on so I decided to stay on the strings.
+for x in "Hackclub":
+    print(x) # Prints each character of the string on a new line
+
+print(" ")
+# To find the length of a string you can use the len() functiom
+x = "Summer of Making"
+print(len(x)) # Prints the length of the string
+
+print(" ")
+
+# To check if a string contains a certain phrase or character 
+Hackinfo = "Hackclub is a community which helps teens create awesome projects"
+print('community' in Hackinfo) # Prints a boolean value which is either ture of false
+# You can also use an 'if' statements to take advantage of the boolean value
+if 'community' in Hackinfo:
+    print("Yes, 'community' is in the string") # This prints the message if there is 'community' in the string
+
+print(" ")
+
+# To check if a string doesn't contain a certain phrase or character
+Hackinfo = "Hackclub is run by many teens to are passantate about coding and helping others"
+print('mean' not in Hackinfo) # Prints a boolean value which is either true or false
+if 'mean' not in Hackinfo:
+    print("No, 'mean' is not in the string") # This prints the message if there is no 'community' in the string
+
+print(" ") 
+
+print("Fourteenth lesson") # The fourteenth lesson talks about string slicing
+# Strng slicing is the way to access parts of a string's character by using indexing
+FruitFav = "My favourite fruit it mango"
+print(FruitFav[2:7])
+
+# If you just want to start on the first character and pick the last character, you can just leave the first number blank
+print(FruitFav[:12]) # This will print the first 12 characters of the string
+# if you want to do the opposte you do the opposite
+print(FruitFav[13:]) # This will print the string starting from the 13th character to the end of the strin
+
+# This is because the syntax of this function is:
+# print(variable[start:end])
+
+# You can also use negative indexing to start from the edn of the string
+print(FruitFav[-5:-1]) # This will start the slice from the last 5 character and end on the last character.
+
+print(" ")
+
+print("Fifteenth lesson") # The fifteenth lesson talks about string modification
+
+# First there is a fucntion called upper() which converts all the characters in the string to uppercase
+UpperFunction = "hackclub"
+print(UpperFunction.upper()) # This will print the string in uppercase
+
+# Next there is a function that does the opposite and converts all the character into lowercase
+LowerFunction = "HACKCLUB"
+print(LowerFunction.lower()) # This will print the string in lowercase
+
+# Then there is a function called strip() which remove any whitespace at the start and end of the string
+StripFunction = "   HackClub   "
+print(StripFunction.strip()) 
+
+# After that there is a function called replace() which replaces the string with another string
+# How this works is that the first value you put in the replace() function is what value your changing and the second value is what your changing it to.
+ReplaceFunction = "Hackclub is a community"
+print(ReplaceFunction.replace("community", "supportive")) # This will replace the word 'community' with 'supportive'
+
+# Next there is a function all split() which splits the string into 2 values
+# For this you need to add a value that you want to split the string
+SplitFunction = "Hackclub is a community"
+print(SplitFunction.split("is")) # This will split the string into 2 values
+
+print(" ")
+
+print("Sixteenth lesson") # The sixteenth lesson teaches about concatenating strings
+
+# To combine stings you use the + operator
+FirstString = "Hackclub is"
+SecondString = "very helpful!"
+CombinedString = FirstString + SecondString # This will combine the 2 strings, FirstString and SecondString
+print(CombinedString) # This will print the combined string
+
+# You can also add the string " " to add a space between the 2 strings
+CombinedString2 = FirstString + " " + SecondString # This will combine the 2 strings with a space in between
+print(CombinedString2) # This prints the CombinedString2 variavble
+
+print(" ")
+
+print("Seventeenth lesson") # The seventeenth lesson teaches about string formatting
+
+# String formatting is used for formatting string so it can be added to a number
+# In python you can use the + operator with the a string variable and a number variable so you need to use the format function
+
+Age = 5
+# Defines age
+CatAge = f"My cat's age is {Age}" # Combines both variables using formatting
+print(CatAge) # Prints formmating
+# This works because:
+# 'f' formats the string and it allows you to add a variable inside '{}' for the text to be finished
+
+# You can use something like this as Place holders
+Price = random.uniform(0.99, 9.99)
+Price2 = random.uniform(10, 15.99)
+Option = f"The price is {Price2}, but with a discount it is {Price}" # This will create a variable with this text
+print(Option) # Prints the variable Option
+# Here is similar to the previous one but uses the variables as placeholders
+# I used the unifrom function for fun with this but there is a problem with this, the numbers are too big
+
+# You can format the variable different ly by using different commands
+Price3 = 8
+Price4 = 96
+Option2 = f"The price is {Price2:.2f}, but with a discount it is {Price:.2f}" # This will format the variable to 2 decimal places
+print(Option2) # Prints the variable Option2
+# This code basically just formats the variable place holders to 2 decimal places with the command '.2f'.
+# You can change the number of decimal places by changing the '2' in '.2f'
+
+print(" ")
+print("Eighteenth lesson") # The eighteenth lesson teaches about escaped characters
+# Escape characters are really interesting. It basiaclly becomes an override feature for the string 
+
+# If you use any illigel (causes an error) characters in a string, for example a single quote in a string that is defined by single quotes, it will come out as an error.
+# To fix this you can use a backslash '\' before the illegel character
+EscapeCharater = 'Hackclub\'s programs are creative' # This string removes the error in front of the backslash and continues on
+print(EscapeCharater) # Prints the variable even with the single quote in the string
+
+# There is even a list of commands that you can use with the backslash
+# \' will make '
+# \" will make "
+# \\ will make \
+# \n will make a new line
+# \r will make a new line (carriage return)(Basically called 'override line')
+# \t will make a tab (big spaces for readability)
+# \b will make a backspace (removes the character before the \b)
+# \f will make a form feed (not really useful for modern code)
+# \ooo will make a character with the octal value ooo (to hard for me))
+# \xhh will make a character with the hex value hh (to hard for me)
+print(" ")
+
+print("Nineteenth lesson") # The nineteenth lesson teaches about string methods
+# There is a lot of methods so I'll have to list and explain them
+
+# capitalize() - This method capitalizes the first character of the string
+CapitalizeMethod = "hackclub is amazing"
+print(CapitalizeMethod.capitalize()) # This will capitalize the first character of the string
+
+# casefold() - This method converts the string to lowercase
+CasefoldMethod = "HACKCLUB IS AMAZING"
+print(CasefoldMethod.casefold()) # This will convert the string to lowercase
+
+# center() - This method centers the string into a new string with characters on both side
+CenterMethod = "Niko"
+print(CenterMethod.center(20, "_")) # This will put 'Niko' into the centre of a new string where '_' centres it
+# The number '20' hold the vale of the length of the new string
+
+# count() = This method counts the number of values in a string
+CountMethod = "Like when I like to like things, I often like to use things that are like similar to what I like"
+print(CountMethod.count("like")) # This will count the number of times 'like' appears in the string
+
+# encode() - This method encodes the string into bypes
+EncodeMethod = "Mango2267"
+print(EncodeMethod.encode()) # This will encode the string into bytes
+
+# endswith() - This method checks at the end of the string to find a punctuation value you have assaigned by outputing a boolean value
+EndswitchMethod = "Summer of Making!"
+print(EndswitchMethod.endswith("!")) # This will output a boolean if there is a '!' in my string
+
+# expandtabs - This method expands the tabs used in a string
+ExpandTabs = "h\ta\tc\tk\t"
+print(ExpandTabs.expandtabs(8)) # This will expand the tabs spaces by the value placed
+
+# find() = This method finds where the first occurance of a value is
+FindMethod = "Summer of Making rewards prizes!"
+print(FindMethod.find("prizes")) # This will find and calcualte the number or character to the string in the string
+# You can also look for letter and search for values in specifc spots
+FindMethod2 = "Huddle around! Get your news here."
+print(FindMethod2.find("u", 15, 34)) # This will find and calcualte the character from the start to the string in the string from the 15th caracter to the end
+# If the method can't find any values in the area, it will output -1
+
+
+
+
