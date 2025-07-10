1d8377d7286004f54590ca4a90061f97ccd5de1a
DuckyBoi-XD <braedenjairsytan@icloud.com>
2025-07-10 23:13:14 +1200

    Started on mini game

--- MiniGame.py
+++ MiniGame.py
@@ -1,1 +1,24 @@
+import random
+
+print("Welcome to the luck room!\nYou get $1000 to play with your luck!")
+
+Game = input("Choose what game you would like to play:\nDice roll, Coin flip, Wheel spin, Card pick")
+
+def DiceRoll():
+    UserBet = input("You have picked " + Game + ".\nPick how much you want to bet!")
+    
+    
+    
+    DiceRoll = random.randrange(1, 7)
+    if UserPick == DiceRoll:
+
+
+if input("Dice roll" or "dice roll"):
+    DiceRoll()
+
+if input("Coin flip" or "coin flip"):
+
+if input("Wheel spin" or "wheel spin"):
+
+if input("Card pick" or "card pick"):
 
