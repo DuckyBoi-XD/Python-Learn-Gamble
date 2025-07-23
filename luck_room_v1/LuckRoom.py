9e31e76ac5ed39b6a7509015fbc6f62bc3712f71
QinCai-rui <140027854+QinCai-rui@users.noreply.github.com>
2025-07-23 13:50:13 +1200

    fix: update welcome message to include starting wallet amount

--- luck_room_v1/LuckRoom.py
+++ luck_room_v1/LuckRoom.py
@@ -1,12 +1,10 @@
 import random # Imports the random package
 
 # Starting game text
-print("Welcome to the luck room")
+print("Welcome to the luck room\nYou start with $1000")
 UserWallet = 1000 # Assigns the users money
 CoinFace = ("heads", "tails") # Assigns coin faces
 CardDeck = ("1 of hearts", "2 of hearts", "3 of heats", "4 of hearts", "5 of hearts", "6 of hearts", "7 of hearts", "8 of hearts", "9 of hearts", "10 of hearts", "jack of hearts", "queen of hearts", "king of hearts", "1 of diamonds", "2 of diamonds", "3 of diamonds", "4 of diamonds", "5 of diamonds", "6 of diamonds", "7 of diamonds", "8 of diamonds", "9 of diamonds", "10 of diamonds", "jack of diamonds", "queen of diamonds", "king of diamonds", "1 of clubs", "2 of clubs", "3 of clubs", "4 of clubs", "5 of clubs", "6 of clubs", "7 of clubs", "8 of clubs", "9 of clubs", "10 of clubs", "jack of clubs", "queen of clubs", "king of clubs", "1 of spades", "2 of spades", "3 of spades", "4 of spades", "5 of spades", "6 of spades", "7 of spades", "8 of spades", "9 of spades", "10 of spades", "jack of spades", "queen of spades", "king of spades")
-
-
 
 # Dice Roll Function
 def DiceRoll(): # Creates DiceRoll function
