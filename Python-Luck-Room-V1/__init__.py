c4bafd12aa9b26305dec1be93f1de4f7ff1d81a6
DuckyBoi-XD <212981554+DuckyBoi-XD@users.noreply.github.com>
2025-07-25 10:07:40 +0000

    chore(release): update RELEASE and version for new GitHub release [Actions]

--- Python-Luck-Room-V1/__init__.py
+++ Python-Luck-Room-V1/__init__.py
@@ -1,2 +1,2 @@
 # This directory is deprecated. The actual package is now in ../luck_room_v1/
-__version__ = '0.1.5'
+__version__ = '0.1.6'
--- Python-Luck-Room-V1/setup.py
+++ Python-Luck-Room-V1/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="luckroom",
-    version="0.1.5",
+    version="0.1.6",
     description="A simple luck based game",
     author="Braedon Sy Tan",
     packages=find_packages(),
