a4ee785e50e88adb2a3391d254a1c88a40a125c1
DuckyBoi-XD <212981554+DuckyBoi-XD@users.noreply.github.com>
2025-07-26 00:05:53 +0000

    chore(release): update RELEASE and version for new GitHub release [Actions]

--- Python-Luck-Room-V1/setup.py
+++ Python-Luck-Room-V1/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="luckroom",
-    version="1.1.8",
+    version="0.1.9",
     description="A simple luck based game",
     author="Braedon Sy Tan",
     packages=find_packages(),
